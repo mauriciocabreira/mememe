56d7fe413e4b098e7f2e531155733b58d6889aaa
Mauricio Cabreira <mauriciocabreira@gmail.com>
2017-05-22 19:31:20 -0300

    Initial Commit

---
+++ Meme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme
+//
+//  Created by Mauricio A Cabreira on 22/05/17.
+//  Copyright © 2017 Mauricio A Cabreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
